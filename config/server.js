e07f81dbb3331ce01935ba85c9a626438355aa34
unknown <bentaouiloussama@gmail.com>
2022-10-26 10:33:59 +0100

    commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2bf7889c6b37170aacedf0b88ce670d6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd8614b78-4fdc-48b6-9261-fa6a877b2882'
+};
